0929fb31d982e34c59b744fdf73ca883b26bb885
chiangfire <chiang-fire@outlook.com>
2019-06-07 22:44:36 +0800

    fix

--- rabbitmq/src/main/java/com/firecode/mqtest/rabbitmq/helloword/Producer.java
+++ rabbitmq/src/main/java/com/firecode/mqtest/rabbitmq/helloword/Producer.java
@@ -21,8 +21,12 @@ public class Producer extends AbstractAmqpClient {
 		for(int i=0;i<size;i++) {
 			/**
 			 * exchange     交换机名称（注意：如果没有指定交换机名称，默认使用 (AMQP default)交换机）
-			 * routingKey   路由的名称（注意：如果没有指定交换机名称，默认使用 (AMQP default)交换机，它会直接将"路由的名称"当队列的名称来用，也就是直接将消息发送到这个"路由的名称"上）
+			 * 
+			 * routingKey   路由的名称（注意：如果没有指定交换机名称，默认使用 (AMQP default)交换机，
+			 *              它会直接将"路由的名称"当队列的名称来用，也就是直接将消息发送到这个"路由的名称"上，前提是这个队列名称已存在）
+			 *              
 			 * props        消息的一些附加属性
+			 * 
 			 * body         消息数据
 			 */
 			channel.basicPublish("",queueName, null, (msg+i).getBytes(StandardCharsets.UTF_8));
