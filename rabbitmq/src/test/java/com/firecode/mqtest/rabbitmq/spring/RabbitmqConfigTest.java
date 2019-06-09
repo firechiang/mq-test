d2de581627907cf9eb5045c3a73b1f72a0bb3433
chiangfire <chiang-fire@outlook.com>
2019-06-09 11:35:09 +0800

    fix

--- rabbitmq/src/test/java/com/firecode/mqtest/rabbitmq/spring/RabbitmqConfigTest.java
+++ rabbitmq/src/test/java/com/firecode/mqtest/rabbitmq/spring/RabbitmqConfigTest.java
@@ -1,9 +1,14 @@
 package com.firecode.mqtest.rabbitmq.spring;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +29,7 @@ public class RabbitmqConfigTest extends BasicTest {
 	 * 创建交换机
 	 */
 	@Test
-	public void adminTest(){
+	public void exchangeTest(){
 		//所有发送到 Direct类型的Exchange(交换机)的消息被转发到RoutingKey中指定的Queue(队列)上
 		admin.declareExchange(new DirectExchange("test.spring.direct", false, false));
 		//所有发送到 Topic类型的Exchange(交换机)的消息被转发到所有关心RoutingKey中指定Topic的Queue(队列)上。
@@ -34,4 +39,46 @@ public class RabbitmqConfigTest extends BasicTest {
 		//注意：fanout类型的Exchange(交换机)转发消息是最快的，性能是最好的
 		admin.declareExchange(new FanoutExchange("test.spring.fanout", false, false));
 	}
+	
+	/**
+	 * 创建队列
+	 */
+	@Test
+	public void queueTest(){
+		admin.declareQueue(new Queue("test.spring.direct.queue",false));
+		admin.declareQueue(new Queue("test.spring.topic.queue",false));
+		admin.declareQueue(new Queue("test.spring.fanout.queue",false));
+	}
+	
+	/**
+	 * 绑定队列到交换机
+	 */
+	@Test
+	public void bindingTest(){
+		// 基本绑定方法
+		admin.declareBinding(new Binding("test.spring.direct.queue",       // 队列的名称
+				                         Binding.DestinationType.QUEUE,    // 绑定类型
+				                         "test.spring.direct",             // 交换机名称
+				                         "test.spring.direct.routing",     // 路由键
+				                         new HashMap<>()));                // 附加属性
+		
+		// 边绑定边创建
+		admin.declareBinding(BindingBuilder.bind(
+				                           new Queue("test.spring.topic.queue",false))  // 队列，不存在会自动创建
+				                           .to(new TopicExchange("test.spring.topic",false,false))// 交换机，不存在会自动创建
+				                           .with("test.spring.topic.*"));// 路由键
+	}
+	
+	/**
+	 * 清空队列里面的消息
+	 */
+	@Test
+	public void purgeQueueTest(){
+		/**
+		 * queueName 队列名称
+		 * noWait    是否需要等待
+		 */
+		admin.purgeQueue("test.spring.topic.queue", false);
+	}
+	
 }
